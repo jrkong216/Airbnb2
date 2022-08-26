5e1c48407ba17baa197b7767189fbbc3f5699654
Jason Kong <jrkong216@gmail.com>
2022-08-26 13:59:40 -0700

    added firstnamelastname seed info

--- backend/db/seeders/20220824201908-demo-user.js
+++ backend/db/seeders/20220824201908-demo-user.js
@@ -7,16 +7,22 @@ module.exports = {
       {
         email: 'demo@user.io',
         username: 'Demo-lition',
+        firstname: 'jason1',
+        lastname: 'kong1',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
         email: 'user1@user.io',
         username: 'FakeUser1',
+        firstname: 'jason2',
+        lastname: 'kong2',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
         email: 'user2@user.io',
         username: 'FakeUser2',
+        firstname: 'jason3',
+        lastname: 'kong3',
         hashedPassword: bcrypt.hashSync('password3')
       }
     ], {});
