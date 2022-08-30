b458ae0d96913f1762c09157c126d1bdd71d96e0
Jason Kong <jrkong216@gmail.com>
2022-08-30 09:23:00 -0700

    Created Bookin Seed Data to populate

---
+++ backend/db/seeders/20220830161945-bookings.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+     await queryInterface.bulkInsert('Bookings', [
+      {
+        userId: 1,
+        spotId: 1,
+        startDate: new Date("2022-08-30"),
+        endDate: new Date("2022-08-31")
+      },
+      {
+        userId: 2,
+        spotId: 2,
+        startDate: new Date("2022-10-01"),
+        endDate: new Date("2022-10-07")
+      },
+      {
+        userId: 3,
+        spotId: 3,
+        startDate: new Date("2022-12-21"),
+        endDate: new Date("2022-12-27"),
+      },
+    ], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+     await queryInterface.bulkDelete('Bookings', null, {});
+  }
+};
