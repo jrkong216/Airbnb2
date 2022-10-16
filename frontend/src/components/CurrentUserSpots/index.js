49c5837995ea2899a00f72ae52cfc72912aed8e0
Jason Kong <jrkong216@gmail.com>
2022-10-15 18:06:11 -0700

    updated current user spots

--- frontend/src/components/CurrentUserSpots/index.js
+++ frontend/src/components/CurrentUserSpots/index.js
@@ -1,14 +1,9 @@
 //component/CURRENT USER/INDEx
 import { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { useParams, useHistory } from "react-router-dom"
-// import {getOneSpot} from '../../store/spotsReducer'
-import {getAllSpots} from '../../store/spotsReducer'
-import { NavLink } from 'react-router-dom';
-import {DeleteSpot} from '../../store/spotsReducer'
-import {getAllReviews} from '../../store/reviewsReducer'
-import {getUserReviews} from '../../store/reviewsReducer'
-import { DeleteReview } from '../../store/reviewsReducer'
+import { useParams } from "react-router-dom"
+import { getAllSpots }  from '../../store/spotsReducer'
+import { getUserReviews } from '../../store/reviewsReducer'
 import "./CurrentUserSpots.css"
 import SpotCard from "../SpotCard"
 
@@ -18,23 +13,10 @@ const GetUserDetails = () => {
     const dispatch = useDispatch()
     const [isLoaded, setIsLoaded] = useState(false)
     const { spotId } = useParams()
-    const history = useHistory()
-    const user = useSelector(state => state.session.user)
-    // console.log("this is user", user)
     const userId = useSelector(state => state.session.user.id)
-
     const spotInfo = useSelector(state => state.spots)
-
     const spotsInfoArray = Object.values(spotInfo)
     const spotsByUserId = spotsInfoArray.filter(spot => spot.ownerId === +userId)
-
-    // console.log("THESE ARE THE spots by user", spotsByUserId)
-
-    const reviewInfo = useSelector(state => state.reviews)
-    const reviewInfoArray = Object.values(reviewInfo)
-    const reviewsBySpotId = reviewInfoArray.filter(review => review.userId === +userId)
-    // console.log("This is the reviewss by spotId", reviewsBySpotId)
-    const reviewByUser = reviewsBySpotId.filter(user => user.userId === +userId)
 
 
     useEffect(() => {
@@ -44,8 +26,6 @@ const GetUserDetails = () => {
     }, [dispatch, spotId])
 
 
-// console.log("THIS IS SPOT INFO", spotInfo)
-
     if (!isLoaded){
         return (<div>Loading...</div>)
         }
@@ -53,72 +33,38 @@ const GetUserDetails = () => {
         if (spotInfo === undefined){
             return null
         }
-        const submitHandler = async (spot) => {
-            // e.preventDefault()
-
-            const payload = {
-               id: spot
-            }
-
-            let createdSpot;
-
-            createdSpot = dispatch(DeleteSpot(payload)).then(() => history.push("/current/user")
-            )
-
-       }
-       const reviewHandler = async (id) => {
-        // e.preventDefault()
-        const payload = {
-            spotId: spotId,
-            reviewId: id
+let spotsOrNot
+        if(spotsByUserId.length === 0){
+            spotsOrNot = (
+                <div>
+                    <h1 className="user-name">My Spots</h1>
+                        <h2>You have no spots!</h2>
+                        {/* <img src= "https://images.pexels.com/photos/45170/kittens-cat-cat-puppy-rush-45170.jpeg?cs=srgb&dl=pexels-pixabay-45170.jpg&fm=jpg"></img> */}
+             </div>
+             )
+        } else {
+         spotsOrNot = (
+            <div>
+                <h1 className="user-name">My Spots</h1>
+                    <div className="user-home-container">
+                        <div className="all-user-card-container">
+                            {spotsByUserId.map((spot) =>
+                                {return (
+                                    <SpotCard key={spot.id} spot={spot}/>
+                                )})
+                            }
+                        </div>
+                    </div>
+            </div>
+         )
         }
-        let reviewToDelete;
-        reviewToDelete = dispatch(DeleteReview(payload))
 
-    }
 
 
     return(
-<div>
-            <h1 className="user-name">My Spots</h1>
-            <div className="user-home-container">
-                <div className="all-user-card-container">
-            {spotsByUserId.map((spot) =>
-                {return (
-                    <SpotCard key={spot.id} spot={spot}/>
-                )})
-            }
-                </div>
-            </div>
-                    <div>
-                    {/* <h2 className="user-name">My Reviews</h2>
-                <div className="user-review-container">
-                    <div className="all-user-review--container">
-                {reviewsBySpotId.map((item) =>
-                {return (
-                    <div key= {item.id}>
-
-                    <div className= "spotReviewName"> Location: {item.Spot.name}</div>
-                    <div className="user-review-data-container">
-                        <i className="fa-solid fa-star fa-xs"></i>
-                    <div className= "spotStars"> {item.stars}</div>
-                    </div>
-                    <div className= "spotReview"> Review: {item.review}</div>
-
-
-                     <button className="user-delete-review-button" onClick= {() => reviewHandler(item.id)}>DELETE THIS Review</button>
-
-                    </div>
-                )}) */}
-                {/* } */}
-                    {/* </div> */}
-                {/* </div> */}
-
-                    </div>
-</div>
-
-
-
+        <>
+        {spotsOrNot}
+        </>
     )
 }
 
