b77ab82f48b34da46efefbb64538fcd65a77d72f
Jason Kong <jrkong216@gmail.com>
2023-01-18 10:29:52 -0800

    Edit a spot up and running and error handlings correct

--- frontend/src/components/UpdateBookingForm/index.js
+++ frontend/src/components/UpdateBookingForm/index.js
@@ -2,20 +2,23 @@ import { useEffect, useState } from "react";
 import { useHistory, useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux"
 import { updateBooking, getBookings, getUserBookings } from '../../store/bookingsReducer';
+import { getAllSpots }  from '../../store/spotsReducer'
 import "./UpdateBookingForm.css"
+import { getAllReviews } from "../../store/reviewsReducer";
 
 function UpdateBookingForm() {
 const moment = require("moment")
 const { bookingId } = useParams()
 const dispatch = useDispatch();
     const history = useHistory();
-    let allBookings = useSelector(state => Object.values(state.bookings));
 
+    let allBookings = useSelector(state => Object.values(state.bookings));
     let currBooking = allBookings.filter(booking => booking.id === parseInt(bookingId));
     console.log("this is currBooking", currBooking)
     let sessionUser = useSelector(state => state.session.user);
-console.log("this is currBooking.startDate", currBooking[0]?.startDate)
-
+console.log("this is currBooking.spotId", currBooking[0]?.spotId)
+    const spotInfo = useSelector(state => state.spots[currBooking[0]?.spotId])
+    console.log("this is spotInfo", spotInfo)
     let [startDate, setStartDate] = useState(moment(currBooking[0]?.startDate.slice(0,10)).format("YYYY-MM-DD"));
     let [endDate, setEndDate] = useState(moment(currBooking[0]?.endDate.slice(0,10)).format("YYYY-MM-DD"));
 
@@ -25,6 +28,7 @@ console.log("this is currBooking.startDate", currBooking[0]?.startDate)
 
 useEffect(() => {
   dispatch(getUserBookings())
+  dispatch(getAllSpots())
 }, [dispatch])
 
 // useEffect(() => {
@@ -99,6 +103,23 @@ catch (res) {
           value={endDate}
         />
       </label>
+      <div className = "bookingpricecontainer">
+                    <div className = "underlinethis">${spotInfo.price} x {(Math.abs(new Date(endDate) - new Date(startDate)))/86400000} nights</div>
+                    <div>${spotInfo.price * (Math.abs(new Date(endDate) - new Date(startDate)))/86400000} </div>
+                </div>
+                <div className = "bookingpricecontainer">
+                    <div className = "underlinethis">Cleaning fee</div>
+                    <div>$80</div>
+                </div>
+
+                <div className = "bookingpricecontainer">
+                    <div className = "underlinethis">Service fee</div>
+                    <div>$50 </div>
+                </div>
+        <div className = "TotalContainer">
+                    <div className = "taxestotal">Total before taxes</div>
+                    <div className = "taxestotal">${80 + 50 + spotInfo.price * (Math.abs(new Date(endDate) - new Date(startDate)))/86400000} </div>
+        </div>
       </div>
 
       <div className="button-Update-container">
